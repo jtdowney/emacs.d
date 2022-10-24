db36f6e7d690585cd34557e3b704b28c741dc935
John Downey <jdowney@gmail.com>
2022-10-23 20:39:29 -0500

    fix spacing

--- init.el
+++ init.el
@@ -6,21 +6,21 @@
 ;; may cause problems on builds of Emacs where its site lisp files aren't
 ;; byte-compiled and we're forced to load the *.el.gz files (e.g. on Alpine)
 (unless (daemonp)
-	(defvar doom--initial-file-name-handler-alist file-name-handler-alist)
-	(setq file-name-handler-alist nil)
-	;; Restore `file-name-handler-alist' later, because it is needed for handling
-	;; encrypted or compressed files, among other things.
-	(defun doom-reset-file-handler-alist-h ()
-		;; Re-add rather than `setq', because changes to `file-name-handler-alist'
-		;; since startup ought to be preserved.
-		(dolist (handler file-name-handler-alist)
-			(add-to-list 'doom--initial-file-name-handler-alist handler))
-		(setq file-name-handler-alist doom--initial-file-name-handler-alist))
-	(add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h)
-	(add-hook 'after-init-hook (lambda ()
-															 ;; restore after startup
-															 (setq gc-cons-threshold 16777216
-																		 gc-cons-percentage 0.1))))
+  (defvar doom--initial-file-name-handler-alist file-name-handler-alist)
+  (setq file-name-handler-alist nil)
+  ;; Restore `file-name-handler-alist' later, because it is needed for handling
+  ;; encrypted or compressed files, among other things.
+  (defun doom-reset-file-handler-alist-h ()
+    ;; Re-add rather than `setq', because changes to `file-name-handler-alist'
+    ;; since startup ought to be preserved.
+    (dolist (handler file-name-handler-alist)
+      (add-to-list 'doom--initial-file-name-handler-alist handler))
+    (setq file-name-handler-alist doom--initial-file-name-handler-alist))
+  (add-hook 'emacs-startup-hook #'doom-reset-file-handler-alist-h)
+  (add-hook 'after-init-hook (lambda ()
+			       ;; restore after startup
+			       (setq gc-cons-threshold 16777216
+				     gc-cons-percentage 0.1))))
 
 ;; Ensure Doom is running out of this file's directory
 (setq user-emacs-directory (file-truename (file-name-directory load-file-name)))
